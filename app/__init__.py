a494e7051215c0cd3c159541cc9bcd722ea420d4
waimea-igdaikee <168692114+waimea-igdaikee@users.noreply.github.com>
2025-05-14 11:57:12 +1200

    Added error page

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from random import randint
 
 app = Flask(__name__)
 
@@ -16,3 +17,12 @@ def test():
 @app.get("/about/")
 def about():
     return render_template("pages/about.jinja")
+
+@app.get("/random/")
+def random():
+    random_number = randint(1,100)
+    return render_template("pages/random.jinja", number=random_number)
+
+@app.errorhandler(404)
+def error404(null):
+    return render_template("pages/error404.jinja")
